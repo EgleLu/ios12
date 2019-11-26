173cfb4d87deaf524fc88eae856fcae5de8b3068
EgleLu <e.luciunaite@gmail.com>
2019-11-26 19:52:24 +0200

    create project

---
+++ projektas1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  projektas1
+//
+//  Created by Egle Luciunaite on 2019-11-26.
+//  Copyright © 2019 Egle Luciunaite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
